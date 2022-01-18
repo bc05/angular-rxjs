94523550549b1e95f4fca04baac7a904dc1c1835
bc05 <odilon.garcez.moraes@gmail.com>
2022-01-17 23:32:09 -0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,28 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { from, of, interval, pipe, range, Subscription, generate } from 'rxjs';
-import { take } from 'rxjs/operators';
-import { AppService } from './app.service';
+import {
+  from,
+  of,
+  interval,
+  pipe,
+  range,
+  Subscription,
+  generate,
+  Observable,
+  merge,
+  forkJoin,
+} from 'rxjs';
+import {
+  combineAll,
+  combineLatestAll,
+  concatAll,
+  concatMap,
+  map,
+  mergeAll,
+  take,
+  tap,
+} from 'rxjs/operators';
+import { AppService, IResponseCep } from './app.service';
 
 @Component({
   selector: 'my-app',
@@ -11,53 +31,73 @@ import { AppService } from './app.service';
 })
 export class AppComponent implements OnInit, OnDestroy {
   name = 'Angular';
+  listAddress = [];
 
-  ceps = [
-    [
-      72898899, 72897002, 72897004, 72897152, 72897154, 72897156, 72897158,
-      72897160, 72897116, 72897118,
-    ],
-    [
-      72897120, 72897122, 72897124, 72897162, 72897126, 72897128, 72897130,
-      72897132, 72897134, 72897136, 72897138,
-    ],
-    [
-      72897140, 72897142, 72897144, 72897146, 72897148, 72897150, 72897200,
-      72897202, 72897204,
-    ],
+  listOfListsCeps = [
+    [72898899, 72897002, 72897004, 72897152, 72897154],
+    [72897156, 72897158, 72897160, 72897116, 72897118],
+    [72897120, 72897122, 72897124, 72897162],
+    [72897126, 72897128, 72897130, 72897132, 72897134, 72897136, 72897138],
+    [72897140, 72897142, 72897144, 72897146, 72897148],
+    [72897150, 72897200, 72897202, 72897204],
+    [72876350],
   ];
-
-  constructor(private appService: AppService) {}
 
   subscription: Subscription = new Subscription();
 
-  ngOnInit(): void {
-    this.subscription.add(
-      from<number[]>([1, 2, 3]).subscribe((value) => console.log('from', value))
-    );
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    const requestObservables: Observable<IResponseCep>[][] =
+      this.listOfListsCeps.map((listCep) =>
+        listCep.map((cep) => this.appService.get(cep))
+      );
 
     this.subscription.add(
-      of<number[]>([1, 2, 3]).subscribe((value) => console.log('of', value))
-    );
-
-    this.subscription.add(
-      interval(1000)
-        .pipe(take(10))
-        .subscribe((value) => console.log('interval', value))
-    );
-
-    this.subscription.add(
-      range(0, 8).subscribe((value) => console.log('range', value))
-    );
-
-    this.subscription.add(
-      generate({
-        initialState: 0,
-        condition: (x) => x < 10,
-        iterate: (x) => x + 1,
-      }).subscribe((value) => console.log('generate', value))
+      from(requestObservables)
+        .pipe(
+          map<
+            Observable<IResponseCep>[],
+            Observable<Observable<IResponseCep>[]>
+          >((chunks) => forkJoin<Observable<IResponseCep>[]>(chunks)),
+          concatMap((chunks) => chunks),
+          concatAll(),
+          map((chunks) => of(chunks)),
+          combineAll()
+        )
+        .subscribe((listAddress) => {
+          this.listAddress = listAddress;
+        })
     );
   }
+
+  // ngOnInit(): void {
+  //   this.subscription.add(
+  //     from<number[]>([1, 2, 3]).subscribe((value) => console.log('from', value))
+  //   );
+
+  //   this.subscription.add(
+  //     of<number[]>([1, 2, 3]).subscribe((value) => console.log('of', value))
+  //   );
+
+  //   this.subscription.add(
+  //     interval(1000)
+  //       .pipe(take(10))
+  //       .subscribe((value) => console.log('interval', value))
+  //   );
+
+  //   this.subscription.add(
+  //     range(0, 8).subscribe((value) => console.log('range', value))
+  //   );
+
+  //   this.subscription.add(
+  //     generate({
+  //       initialState: 0,
+  //       condition: (x) => x < 10,
+  //       iterate: (x) => x + 1,
+  //     }).subscribe((value) => console.log('generate', value))
+  //   );
+  // }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,5 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface IResponseCep {
+  bairro: string;
+  cep: string;
+  complemento: string;
+  ddd: string;
+  gia: string;
+  ibge: string;
+  localidade: string;
+  logradouro: string;
+  siafi: string;
+  uf: string;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -7,8 +21,8 @@ import { Injectable } from '@angular/core';
 export class AppService {
   constructor(private http: HttpClient) {}
 
-  buscar(cep: number) {
+  get(cep: number): Observable<IResponseCep> {
     const url = `https://viacep.com.br/ws/${cep}/json/`;
-    return this.http.get(url);
+    return this.http.get<IResponseCep>(url);
   }
 }
